f89594eb4368733668ffd4374dc11e33f3b71630
Natasha Gaye <natashagaye11@gmail.com>
2025-03-27 13:31:36 -0500

    defined student classes, behaviors, and methods

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,3 @@
 # add your get_student_with_more_classes function here!
+# input two args, 2 students
+# output student 
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,25 @@
 # add your Student class here!
+class Student:
+    def __init__(self, name, class_year, classes):
+        self.name = name
+        self.class_year = class_year
+        self.classes = classes
+        self.num_classes = len(classes)
+
+    def add_class(self, class):
+        '''
+            This function has 2 paramenters. The first argument helps
+            us access all attributes(i.e. name, classes) in the constructor.
+            It should append the second argument to the self.classes list
+        '''
+        class.append(classes)
+    def get_num_classes(self):
+        '''
+            This function has 1 paramenter. It helps us access all attributes(i.e. num_classes) 
+            in the constructor. It should return the value of self.num_classes
+        '''
+    def summary(self):
+        '''
+            This function has 1 paramenter. It helps us access all attributes(i.e. name, class_year) 
+            in the constructor. It should return a string => example f"Samara is a junior enrolled in 7 classes"
+        '''
