8c5f218f133544a86e8b87747d0a16fa2182a4d2
Natasha Gaye <natashagaye11@gmail.com>
2025-03-31 18:03:03 -0500

    completed activity and pushing to see Laura's added file

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,3 +1,10 @@
 # add your get_student_with_more_classes function here!
-# input two args, 2 students
-# output student 
+# input two args , in this case it's 2 instances of Student class (or 2 objects)
+# output name of student with the most classes
+# declare the function
+# declare variable called to store the name of the student with the most classes
+# get the num of classes for first student
+# get the num of classes for second student
+# compare the two with a condition
+# based on the condition, update the value of student name variable
+# return student name
--- activity/student.py
+++ activity/student.py
@@ -6,20 +6,24 @@ class Student:
         self.classes = classes
         self.num_classes = len(classes)
 
-    def add_class(self, class):
+    def add_class(self, class_name):
         '''
             This function has 2 paramenters. The first argument helps
             us access all attributes(i.e. name, classes) in the constructor.
-            It should append the second argument to the self.classes list
+            It should append the second argument, in this case it's called class, to the self.classes list
         '''
-        class.append(classes)
+        self.classes.append(class_name)
+
     def get_num_classes(self):
         '''
             This function has 1 paramenter. It helps us access all attributes(i.e. num_classes) 
             in the constructor. It should return the value of self.num_classes
         '''
+        return self.num_classes
+    
     def summary(self):
         '''
             This function has 1 paramenter. It helps us access all attributes(i.e. name, class_year) 
             in the constructor. It should return a string => example f"Samara is a junior enrolled in 7 classes"
         '''
+        return self.summary (f"{self.name} is a {self.class_year} enrolled in {self.num_classes} classes")
